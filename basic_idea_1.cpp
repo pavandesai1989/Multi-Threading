c8cc47746bcc6070852fee18962991a26362560d
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2022-12-27 20:32:41 +0530

    Update basic_idea_1.cpp

--- basic_idea_1.cpp
+++ basic_idea_1.cpp
@@ -16,6 +16,20 @@ WAYS TO CREATE THREADS IN C++11
 4. Member Functions
 5. Static Member functions
 
+In every C++ application there is one default main thread i.e. main() function. 
+In C++ 11 we can create additional threads by creating objects of std::thread class.
+Each of the std::thread object can be associated with a thread.
+
+What std::thread accepts in constructor ?
+We can attach a callback with the std::thread object, that will be executed when this new thread starts. These callbacks can be,
+
+1.) Function Pointer
+2.) Function Objects
+3.) Lambda functions
+
+Thread objects can be created like this,
+std::thread thObj(<CALLBACK>);
+
 */
 
 #include<thread>
